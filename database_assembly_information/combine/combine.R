5be7c87ae3c3865d21c996e37caf7f787e5e5a9d
wcornwell <wcornwell@gmail.com>
2019-01-22 12:58:11 +1100

    using correct source file

--- database_assembly_information/combine/combine.R
+++ database_assembly_information/combine/combine.R
@@ -1,11 +1,9 @@
 library(tidyverse)
 
-gf<-read_csv("database_assembly_information/data_with_sources/complete_growthform.csv")
+gf<-read_csv("database_assembly_information/data_with_sources/consensus_species_growthform.csv")
 wood<-read_csv("database_assembly_information/data_with_sources/GlobalWoodinessDatabase.csv")
 nfix<-read_csv("database_assembly_information/data_with_sources/Werner_NFix.csv")
 
-head(wood)
-head(gf)
 
 wood<-rename(wood,sp=gs)
 nfix<-rename(nfix,sp=Species)
@@ -14,6 +12,6 @@ left_join(gf,wood) %>%
   left_join(nfix) %>%
   select(sp,support,woodiness,Data_fixing) %>%
   rename(nfixing=Data_fixing) %>%
-  write_csv("growth_form.csv") ->out
+  write_csv("growth_form.csv") 
 
 
